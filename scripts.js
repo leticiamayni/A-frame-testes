180736df729934ceb37937238973ab65a18f4276
Leticia Mayni <leticiamayni1@gmail.com>
2021-06-17 14:33:36 -0300

    video autoplay script

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 //-- Autoplay Videos -->
 var vidList = document.querySelectorAll("#camila_corte4, #camila_corte5, #camila_corte3");
 for (let i = 0; i < vidList.length; i++) {
-    vidList[i].play();
+    vidList[i].autoplay = true;
+    vidList[i].load();
 }
 
 
