484bb6f580b0898017f517e44c47e07196a612ae
Leticia Mayni <leticiamayni1@gmail.com>
2021-06-17 14:23:06 -0300

    Zoom Scene

---
+++ scripts.js
@@ -0,0 +1,7 @@
+//-- Autoplay Videos -->
+var vidList = document.querySelectorAll("#camila_corte4, #camila_corte5, #camila_corte3");
+for (let i = 0; i < vidList.length; i++) {
+    vidList[i].play();
+}
+
+
